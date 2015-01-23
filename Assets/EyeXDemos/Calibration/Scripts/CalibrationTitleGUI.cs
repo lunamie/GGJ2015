12d62802985253937ba9b911e94ff302ebc920f7
Yuki Numakura <aqua.yuki.1106@gmail.com>
2015-01-23 22:19:39 +0900

    project追加

---
+++ Assets/EyeXDemos/Calibration/Scripts/CalibrationTitleGUI.cs
@@ -0,0 +1,124 @@
+﻿//-----------------------------------------------------------------------
+// Copyright 2014 Tobii Technology AB. All rights reserved.
+//-----------------------------------------------------------------------
+
+using UnityEngine;
+
+/// <summary>
+/// Script that displays GUI buttons for recalibration and to test calibration.
+/// <para>
+/// Note that the Application.runInBackground property is set to true
+/// while waiting for the recalibration to finish. If you use this script 
+/// in a game, make sure that the background running does not cause any 
+/// unpleasant side effects. 
+/// </para>
+/// </summary>
+public class CalibrationTitleGUI : MonoBehaviour
+{
+    private EyeXHost _host;
+    private WaitingState _waitingState = WaitingState.NotWaiting;
+    private bool _originalRunInBackgroundState;
+
+    private enum WaitingState
+    {
+        NotWaiting = 0,
+        WaitingForCalibrationToStart,
+        WaitingForCalibrationToFinish
+    }
+
+    private void Start()
+    {
+        _host = EyeXHost.GetInstance();
+        _host.Start();
+
+        _originalRunInBackgroundState = Application.runInBackground;
+    }
+
+    private void OnDisable()
+    {
+        StopWaitingForCalibration();
+    }
+
+    private void OnGUI()
+    {
+        if (!IsSupportedEngineVersion())
+        {
+            var content = new GUIContent("This functionality requires EyeX Engine version 1.1 or higher.");
+
+            // Create the font style.
+            var style = new GUIStyle();
+            style.alignment = TextAnchor.MiddleCenter;
+            style.fontSize = 18;
+            style.normal.textColor = new Color(0.953f, 0.569f, 0.039f, 1f);
+
+            // Calculate the boundaries.
+            var height = style.CalcHeight(content, 700) + 30;
+            var bounds = new Rect((Screen.width - 700) / 2f, Screen.height / 2f - (height / 2), 700, height);
+
+            // Draw the label.
+            GUI.Label(bounds, content, style);
+            return;
+        }
+
+        // Draw the box surrounding the buttons.
+        GUI.Box(new Rect(10, 10, 170, 110), "Calibration");
+
+        // Draw the "Recalibrate" button.
+        if (GUI.Button(new Rect(20, 40, 150, 30), "Recalibrate"))
+        {
+            StartWaitingForCalibration();
+
+            _host.LaunchRecalibration();
+        }
+
+        // Draw the "Test calibration" button.
+        if (GUI.Button(new Rect(20, 80, 150, 30), "Test calibration"))
+        {
+            _host.LaunchCalibrationTesting();
+        }
+    }
+
+    private void Update()
+    {
+        if (_waitingState == WaitingState.WaitingForCalibrationToStart
+            && _host.EyeTrackingDeviceStatus == EyeXDeviceStatus.Pending)
+        {
+            print("Waiting for calibration to finish");
+            _waitingState = WaitingState.WaitingForCalibrationToFinish;
+        }
+        else if (_waitingState == WaitingState.WaitingForCalibrationToFinish
+                 && _host.EyeTrackingDeviceStatus == EyeXDeviceStatus.Tracking)
+        {
+            print("Calibration finished. Bring back focus to application");
+            WindowHelpers.ShowCurrentWindow();
+
+            StopWaitingForCalibration();
+        }
+    }
+
+    private void StartWaitingForCalibration()
+    {
+        _originalRunInBackgroundState = Application.runInBackground;
+
+        // Set runInBackground to true to be able to wait for calibration to finish.
+        Application.runInBackground = true;
+
+        print("Waiting for calibration to start");
+        _waitingState = WaitingState.WaitingForCalibrationToStart;
+    }
+
+    private void StopWaitingForCalibration()
+    {
+        // Reset runInBackground to its original value when the waiting is over.
+        Application.runInBackground = _originalRunInBackgroundState;
+
+        _waitingState = WaitingState.NotWaiting;
+    }
+
+    private bool IsSupportedEngineVersion()
+    {
+        // The EyeX Engine version need to be equal to or higher than 1.1.
+        var version = _host.EngineVersion;
+        return version != null && version.Major >= 1 && version.Minor >= 1;   
+    }
+}
---
+++ Assets/EyeXDemos/EyePosition/Scripts/FollowEyePosition.cs
@@ -0,0 +1,93 @@
+﻿//-----------------------------------------------------------------------
+// Copyright 2014 Tobii Technology AB. All rights reserved.
+//-----------------------------------------------------------------------
+
+using UnityEngine;
+using UnityEngine.Internal;
+
+/// <summary>
+/// Unity script for a game object that follows the position of an eye in 3D space.
+/// </summary>
+[RequireComponent(typeof(EyePositionDataComponent), typeof(Renderer))]
+public class FollowEyePosition : MonoBehaviour
+{
+    // Scale: 1 mm maps to 0.001 units in world space
+    private const float Scale = 1 / 1000.0f;    
+
+    private EyePositionDataComponent _eyePositionDataComponent;
+    private Renderer _rendererComponent;
+    private float _accumulatedTime;
+
+    /// <summary>
+    /// Choice of eye position to follow, the position of the right or the left eye.
+    /// </summary>
+    public Eye eyeToFollow = Eye.Left;
+
+    /// <summary>
+    /// The threshold in milliseconds to be reached before the eye is hidden
+    /// after eye tracking has been lost. This prevents flickering 
+    /// if the calibration is suboptimal.
+    /// </summary>
+    [Range(0, 1000)]
+    public float thresholdMs = 100f;
+
+    /// <summary>
+    /// Represents an eye.
+    /// </summary>
+    public enum Eye
+    {
+        Left,
+        Right
+    }
+
+    protected void Start()
+    {
+        _eyePositionDataComponent = GetComponent<EyePositionDataComponent>();
+        _rendererComponent = GetComponent<Renderer>();
+        _rendererComponent.enabled = false;
+        _accumulatedTime = 0f;
+    }
+
+    protected void Update()
+    {
+        // Get the latest eye position data for both eyes from the eye position data component.
+        var eyePosition = _eyePositionDataComponent.LastEyePosition;       
+        var renderEye = false;
+
+        if (eyePosition != null && eyePosition.IsValid)
+        {
+            // Get the eye position of the selected eye to follow.
+            var singleEyePosition = eyeToFollow == Eye.Left ? eyePosition.LeftEye : eyePosition.RightEye;
+            if (singleEyePosition.IsValid)
+            {
+                // Show the game object.
+                _rendererComponent.enabled = true;
+
+                // Move the game object to the current position of the selected eye to follow
+                transform.position = new Vector3(-singleEyePosition.X * Scale, singleEyePosition.Y * Scale,
+                    singleEyePosition.Z * Scale);
+
+                // Reset the accumulated time.
+                _accumulatedTime = 0f;
+
+                // Show the eyes.
+                renderEye = true;
+            }
+        }
+
+        if (!renderEye)
+        {
+            // Increase the accumulated time with
+            // the time elapsed since the last update.
+            _accumulatedTime += Time.deltaTime;
+
+            // Reached the threshold to hide the game object?
+            if (_accumulatedTime >= (thresholdMs / 1000f))
+            {
+                // If there is no position for the eye to follow, 
+                // for example during blink: hide the game object.
+                _rendererComponent.enabled = false;
+            }            
+        }
+    }
+}
