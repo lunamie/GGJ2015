35528d724471289f844d301b0f4fc4e055bbfa57
ShinodaKen <shinoda.ken@gmail.com>
2015-01-24 19:53:42 +0900

    カメラコントローラの追加

---
+++ Assets/lunamie/Script/Camera_CameraCtrl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera_CameraCtrl : Photon.MonoBehaviour {
+
+	Vector3 oldMousePosition = new Vector3();
+	public float speed = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		oldMousePosition = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newMousePosition = Input.mousePosition;
+		Vector3 diff = (newMousePosition - oldMousePosition);
+		diff.z = 0;
+		float m = diff.magnitude * speed;
+		diff.Normalize();
+		diff.Scale(new Vector3(m,m,0));
+		oldMousePosition = newMousePosition;
+
+		this.transform.position += diff;
+	}
+
+	void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info) {
+		if (stream.isWriting) {
+			//現在地と角度を送信
+			stream.SendNext (this.transform.position);
+		} else {
+			this.transform.position = (Vector3)stream.ReceiveNext ();
+		}
+	}
+}
