79ae9f29d73f2236269e7eefe58509e888d5daf3
ShinodaKen <shinoda.ken@gmail.com>
2015-01-25 12:04:33 +0900

    カメラコントローラの追加

--- Assets/lunamie/Script/Camera_CameraCtrl.cs
+++ Assets/lunamie/Script/Camera_CameraCtrl.cs
@@ -1,35 +1,47 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Camera_CameraCtrl : Photon.MonoBehaviour {
-
-	Vector3 oldMousePosition = new Vector3();
-	public float speed = 0.2f;
-
+public class Camera_CameraCtrl : MonoBehaviour {
+	PhotonView m_PhotonView;
+	Vector3 m_velocity = Vector3.zero;
+	float max   = 2.0f;
+	float decay = 0.2f;
+	float force = 0.2f;
 	// Use this for initialization
+	void Awake()
+	{
+		m_PhotonView = GetComponent<PhotonView>();
+	}
+
 	void Start () {
-		oldMousePosition = Input.mousePosition;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 newMousePosition = Input.mousePosition;
-		Vector3 diff = (newMousePosition - oldMousePosition);
-		diff.z = 0;
-		float m = diff.magnitude * speed;
-		diff.Normalize();
-		diff.Scale(new Vector3(m,m,0));
-		oldMousePosition = newMousePosition;
-
-		this.transform.position += diff;
-	}
-
-	void OnPhotonSerializeView (PhotonStream stream, PhotonMessageInfo info) {
-		if (stream.isWriting) {
-			//現在地と角度を送信
-			stream.SendNext (this.transform.position);
-		} else {
-			this.transform.position = (Vector3)stream.ReceiveNext ();
+		float InputX = Input.GetAxis("Horizontal");
+		float InputY = Input.GetAxis("Vertical");
+		if (m_PhotonView.isMine == false && PhotonNetwork.connected) {
+			if (InputX!=0 || InputY!=0) {
+				m_velocity += new Vector3(InputX * force, InputY * force, 0);
+				if(m_velocity.sqrMagnitude>max)
+				{
+					float m = max/m_velocity.sqrMagnitude;
+					m_velocity.Normalize();
+					m_velocity.Scale(new Vector3(m,m,0));
+				}
+			}
+			else {
+				m_velocity.Normalize();
+				m_velocity.Scale(new Vector3(decay,decay,0));
+			}
+			transform.position += m_velocity;
+			m_PhotonView.RPC ("Move", PhotonTargets.Others, transform.position);
 		}
+	}
+	
+	[RPC]
+	public void Move(Vector3 pos)
+	{
+		this.transform.position = pos;
 	}
 }
